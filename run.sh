225f5c095f2e2f1f47f837f20b95cc104f76f23d
Shin, Hyeon-Kyeong <35495333+gusrud1103@users.noreply.github.com>
2021-11-14 22:53:53 +0900

    Add files via upload

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ echo "         Construct LibriPhrase         "
 echo "======================================="
 
 # diffspk_all mode
-# python3 libriphrase.py  \
-# --libripath /home/hkshin/server_hdd/Database/LibriSpeech_clean_wav/  \
-# --newpath /home/hkshin/server_hdd/Database/LibriPhrase_diffspk_all/  \
-# --wordalign ./data/librispeech_other_train_500h_all_utt.csv  \
-# --output ./data/libriphrase_diffspk_all.csv  \
-# --numpair 3 --maxspk 1166 --maxword 4 --mode diffspk_all
+python3 libriphrase.py  \
+--libripath /home/hkshin/server_hdd/Database/LibriSpeech_clean_wav/  \
+--newpath /home/hkshin/server_hdd/Database/LibriPhrase_diffspk_all/  \
+--wordalign ./data/librispeech_other_train_500h_all_utt.csv  \
+--output ./data/libriphrase_diffspk_all.csv  \
+--numpair 3 --maxspk 1166 --maxword 4 --mode diffspk_all
 
 # samespk_easy mode
 # python3 libriphrase.py  \
@@ -31,10 +31,10 @@ echo "======================================="
 # --numpair 3 --maxspk 1166 --maxword 4 --mode diffspk_easy
 
 # diffspk_hard mode
-python3 libriphrase.py  \
---libripath /home/hkshin/server_hdd/Database/LibriSpeech_clean_wav/  \
---newpath /home/hkshin/server_hdd/Database/LibriPhrase_diffspk_hard/  \
---wordalign ./data/librispeech_other_train_500h_all_utt.csv  \
---output ./data/libriphrase_diffspk_hard.csv  \
---numpair 3 --maxspk 1166 --maxword 4 --mode diffspk_hard
+# python3 libriphrase.py  \
+# --libripath /home/hkshin/server_hdd/Database/LibriSpeech_clean_wav/  \
+# --newpath /home/hkshin/server_hdd/Database/LibriPhrase_diffspk_hard/  \
+# --wordalign ./data/librispeech_other_train_500h_all_utt.csv  \
+# --output ./data/libriphrase_diffspk_hard.csv  \
+# --numpair 3 --maxspk 1166 --maxword 4 --mode diffspk_hard
 
